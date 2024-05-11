3fe0cece48d388e9d78b4abc7c21417e6a500cce
Nesxynz <79847800+TafGaming@users.noreply.github.com>
2024-05-11 22:00:23 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Aplikasi Nilai
+## CRUD kelas
+Membuat CRUD
+
+Create
+Read
+Update
+Delete
+
+Dengan kolom database seperti berikut
+id
+kelas
+kapasitas
+terisi
---
+++ home.php
@@ -0,0 +1,18 @@
+<div class="card">
+    <div class="card-body">
+        <p>
+            <img src="images/INDO.jpg" class="rounded float-end" alt="Indonesia" height="250px">
+            Indonesia, dengan nama resmi Republik Indonesia, adalah sebuah negara kepulauan di Asia Tenggara yang dilintasi garis 
+            khatulistiwa dan berada di antara daratan benua Asia dan Oseania sehingga dikenal sebagai negara lintas benua, serta 
+            antara Samudra Pasifik dan Samudra Hindia.
+
+
+            Indonesia merupakan negara terluas ke-14 sekaligus negara kepulauan terbesar di dunia dengan luas wilayah sebesar 1.904.569 km²
+            , serta negara dengan pulau terbanyak ke-6 di dunia, dengan jumlah 17.504 pulau. Nama alternatif yang dipakai untuk 
+            kepulauan Indonesia disebut Nusantara. Selain itu, Indonesia juga menjadi negara berpenduduk terbanyak ke-4 di dunia dengan
+             penduduk mencapai 277.749.853 jiwa pada tahun 2022, serta negara dengan penduduk beragama Islam terbanyak kedua di dunia 
+             setelah Pakistan, dengan penganut lebih dari 238.875.159 jiwa atau sekitar 86,9%. Indonesia adalah negara multiras, 
+             multietnis, dan multikultural di dunia, seperti halnya Amerika Serikat
+        </p>
+    </div>
+</div>
